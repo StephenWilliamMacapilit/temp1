227746ef377a088b2517e42173f3ef1df2136ae2
StephenWilliamMacapilit <120321161+StephenWilliamMacapilit@users.noreply.github.com>
2025-09-17 20:55:24 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World! My first deployed web app 🚀");
+  res.send("Hello Nath! This will be our testing grounds for now.");
+  res.send("Our tech stack is as follows:");
+  res.send("Fronted = HTML & CSS (maybe PHP or REACT");
+  res.send("Backend = Node.js");
+  res.send("Database = Postgres");
+  res.send("Server = Render (Subject to change)");
 });
 
 app.listen(PORT, () => {
